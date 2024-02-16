2f9ddd637ce587fbed2d1900ffbae27c57a7c2d4
dennisvinnikov11 <146028500+dennisvinnikov11@users.noreply.github.com>
2024-02-16 13:29:49 -0800

    Create port_forward.sh
    Script that configures port forwarding

---
+++ network/port_forward.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# this script adds or deletes port forwarding, it's still raw and will be updated
+read -p "Would you like to add or remove " op
+read -p "Enter zone " zone
+read -p "Enter first port " p1
+read -p "Enter second port " p2
+read -p "Enter protocol " prt
+read -p "Enter address " addr
+
+sudo firewall-cmd --zone=$zone --permanent --$op-forward-port=port=$p1:proto=$prt:toport=$p2:toaddr=$addr
+sudo firewall-cmd --reload
+sudo firewall-cmd --list-all --zone=$zone
